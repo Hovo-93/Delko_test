90762bb3352629e4f5a66e354ba2710f65a8876c
Hovo-93 <69504228+Hovo-93@users.noreply.github.com>
2023-10-04 15:12:28 +0300

    added end date unput logic

---
+++ EndDateInput.py
@@ -0,0 +1,16 @@
+from DateInputMixin import DateInput
+import datetime
+import locale
+
+locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+
+
+class EndInput(DateInput):
+
+    def __init__(self):
+        super().__init__()
+        self.end_date = None
+
+    def process(self):
+        self.end_date = datetime.datetime(year=self.get_year(), month=self.get_month(), day=self.get_day())
+        return self.end_date
